0fb9d3aaaed4a29529be353b7552fcc508784d1f
Ngizeh Mwas <edngize@gmail.com>
2020-12-07 20:21:39 +0300

    changes

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -15,53 +15,50 @@
 		</header>
 		<main class="bg-gray-200 space-y-8" :class="{ 'h-screen' : !products.length, 'h-full' : products.length > 0}">
 			<div class="flex flex-col-reverse md:w-10/12 md:mx-auto md:flex md:flex-row pt-8">
-           <!-- <div v-if="loading">
-              Loading....
-           </div> -->
-           <div class="w-full px-6 md:w-1/2">
-           	<p class="py-6 font-bold ">
-           		Found {{ products.length }} in the search term {{ lastSearch }}
-           	</p>
-           	<div v-for="(product , id) in products" :key=id class="bg-white py-2 px-4 md:py-6 md:px-12 mb-6 shadow rounded">
-           		<div class="lg:flex">
-           			<div>
-           				<img :src="product.thumb" class="px-2 py-3 md:mr-6 md:py-4 md:w-64 md:h-48" alt="">
-           			</div>
-           			<div class="space-y-2 mb-2 md:flex md:flex-col text-center md:justify-center ">
-           				<p class="md:py-2">{{ product.title }}</p>
-           				<p class="md:py-2 text-blue-700 md:text-xl font-bold">{{ product.price | totally }}</p>
-           				<button @click="addToCart(product)" class="text-sm px-2 py-1 bg-blue-500 text-white md:px-4 rounded">Add to cart</button>
-           			</div>
-
-           		</div>
-           	</div>
-           </div>
-           <div class="w-full px-10 md:w-1/2 flex flex-col text-gray-800">
-           	<div class="border-b border-gray-500 mb-4 mt-6">
-           		<h2 class="uppercase font-bold lg:text-2xl pb-2">
-           			shopping cart
-           		</h2>
-           		<ul>
-           			<li v-for="(item , id ) in cart" :key="id" class="py-2">
-           				{{ item.title }}
-           				<p class="py-2">{{ item.price | totally }} x {{ item.quantity }} |
-           					<button @click="inc(item)" class="font-bold ml-2 bg-blue-500 px-2 text-xs hover:text-white rounded">+</button>
-           					<button @click="dec(item)" class="font-bold ml-2 bg-blue-500 px-2 text-xs hover:text-white rounded">&minus;</button>
-           				</p>
-           			</li>
-           		</ul>
-           	</div>
-           	<span v-if="cart.length" class="font-semibold text-lg text-right">{{ total | totally }}</span>
-           	<span v-if="!cart.length" class="font-semibold lg:text-lg"> No item in the Cart </span>
-           </div>
-        </div>
-        <div class="bg-gray-900 py-4 flex justify-center text-gray-400 text-sm sm:text-lg">
-        	<div>
-        		Workcation &copy; {{ date.getFullYear('Y')}} . All rights Preserved. Developed by <a href="https://github.com/Ngizeh">Edward Mwangi</a>
-        	</div>
-        </div>
-     </main>
-  </div>
+				<div class="w-full px-6 md:w-1/2">
+					<p class="py-6 font-bold ">
+						Found {{ products.length }} in the search term {{ lastSearch }}
+					</p>
+					<div v-for="(product , id) in products" :key=id class="bg-white py-2 px-4 md:py-6 md:px-12 mb-6 shadow rounded">
+						<div class="lg:flex">
+							<div>
+								<img :src="product.thumb" class="px-2 py-3 md:mr-6 md:py-4 md:w-64 md:h-48" alt="">
+							</div>
+							<div class="space-y-2 mb-2 md:flex md:flex-col text-center md:justify-center ">
+								<p class="md:py-2">{{ product.title }}</p>
+								<p class="md:py-2 text-blue-700 md:text-xl font-bold">{{ product.price | totally }}</p>
+								<button @click="addToCart(product)" class="text-sm px-2 py-1 bg-blue-500 text-white md:px-4 rounded">Add to cart</button>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="w-full px-10 md:w-1/2 flex flex-col text-gray-800">
+					<div class="border-b border-gray-500 mb-4 mt-6">
+						<h2 class="uppercase font-bold lg:text-2xl pb-2">
+							shopping cart
+						</h2>
+						<ul>
+							<li v-for="(item , id ) in cart" :key="id" class="py-2">
+								{{ item.title }}
+								<p class="py-2">{{ item.price | totally }} x {{ item.quantity }} |
+									<button @click="inc(item)" class="font-bold ml-2 bg-blue-500 px-2 text-xs hover:text-white rounded">+</button>
+									<button @click="dec(item)" class="font-bold ml-2 bg-blue-500 px-2 text-xs hover:text-white rounded">&minus;</button>
+								</p>
+							</li>
+						</ul>
+					</div>
+					<span v-if="cart.length" class="font-semibold text-lg text-right">{{ total | totally }}</span>
+					<span v-if="!cart.length" class="font-semibold lg:text-lg"> No item in the Cart </span>
+				</div>
+			</div>
+			<div class="bg-gray-900 py-4 flex justify-center text-gray-400 text-sm sm:text-lg">
+				<div>
+					<span>Workcation &copy; {{ date.getFullYear('Y') }} . All rights Preserved. Developed by</span>
+					<a href="https://github.com/Ngizeh">Edward Mwangi</a>
+				</div>
+			</div>
+		</main>
+	</div>
 </template>
 
 <script>
@@ -75,69 +72,64 @@
 				cart : [],
 				search : 'Cat',
 				lastSearch : '',
-				date : new Date()
-        // loading : false
-     }
-  },
-  methods : {
-  	addToCart(product)
-  	{
-  		this.total += product.price;
-  		let found = false;
-  		for (let i = 0; i < this.cart.length; i++)
-  		{
-  			if(this.cart[i].id === product.id){
-  				this.cart[i].quantity++ ;
-  				found = true;
-  			}
-  		}
-  		if (!found)
-  		{
-  			this.cart.push({
-  				id : product.id,
-  				title : product.title,
-  				price : product.price,
-  				quantity : 1,
-  			})
-  		}
-  		scrollTo(0, 300);
-  	},
-  	inc(item){
-  		item.quantity++
-  		this.total += item.price;
-  		if(item.quantity == 0){
-  			return this.total = 0
-  		}
-  	},
-  	dec(item){
-  		item.quantity--
-  		this.total -= item.price;
-  		if(item.quantity == 0){
-  			return this.cart.pop()
-  		}
-  	},
-  	onSubmit(){
-  		var app = this;
-  		app.products = [];
-  		app.lastSearch = app.search
-            // app.loading = true;
-            axios('/search.json')
-            .then(function(response) {
-            	const products = response.data
-            	app.products = products.filter((product) => product.title.includes(app.lastSearch))
-                    //  app.loading = false;
-                 })
-         }
-      },
-      filters : {
-      	totally(price) {
-      		return `Ksh. ${price.toFixed(2)}`
-      	}
-      },
-      created() {
-      	this.onSubmit()
-      }
-   }
+				date : new Date
+			}
+		},
+		methods : {
+			addToCart(product){
+				this.total += product.price;
+				let found = false;
+				for (let i = 0; i < this.cart.length; i++)
+				{
+					if(this.cart[i].id === product.id){
+						this.cart[i].quantity++ ;
+						found = true;
+					}
+				}
+				if (!found)
+				{
+					this.cart.push({
+						id : product.id,
+						title : product.title,
+						price : product.price,
+						quantity : 1,
+					})
+				}
+				scrollTo(0, 300);
+			},
+			inc(item){
+				item.quantity++
+				this.total += item.price;
+				if(item.quantity == 0){
+					return this.total = 0
+				}
+			},
+			dec(item){
+				item.quantity--
+				this.total -= item.price;
+				if(item.quantity == 0){
+					return this.cart.pop()
+				}
+			},
+			onSubmit(){
+				var app = this;
+				app.products = [];
+				app.lastSearch = app.search;
+				axios('/search.json').then(function(response) {
+					const products = response.data;
+					app.products = products.filter((product) => product.title.includes(app.lastSearch))
+				});
+			}
+		},
+		filters : {
+			totally(price) {
+				return `Ksh. ${price.toFixed(2)}`
+			}
+		},
+		created() {
+			this.onSubmit()
+		}
+	}
 </script>
 
 <style src="../assets/tailwind.css">
